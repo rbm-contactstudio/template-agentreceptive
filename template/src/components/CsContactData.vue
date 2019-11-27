fd2960fc27c0ffa387d0b12e00e77e8bdde5635b
rbm-contactstudio <50591997+rbm-contactstudio@users.noreply.github.com>
2019-11-27 16:36:54 -0300

    Update CsContactData.vue

--- template/src/components/CsContactData.vue
+++ template/src/components/CsContactData.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <CsForm :form="items" :form-data="getContact.data" :columns="2" />
+    <div class="row">
+      <div class="col-sm-4">
+        <CsInputCEP :field="fieldCep" :searchable="false" @AddressFounded="populateCEP" @AddressSelected="populateCEP" />
+      </div>
+    </div>
     <div class="text-center">
       <div class="btn btn-primary" @click="updateDataContact">Atualizar Dados</div>
     </div>
@@ -9,11 +14,12 @@
 
 <script>
   import { mapActions, mapGetters } from 'vuex';
-  import { CsForm, CsEvents } from '@contactstudio/agent-tools';
+  import { CsForm, CsInputCEP, CsEvents } from '@contactstudio/agent-tools';
 
   export default {
     components: {
       CsForm,
+      CsInputCEP
     },
     props: {
       items: {
@@ -28,14 +34,29 @@
     },
     computed: Object.assign({},
       mapGetters('lib/', ['getContact']),
-      {}
+      {
+        fieldCep(){
+          return {
+            label: 'CEP',
+            valid: true,
+            value: this.getContact.data.CEP,
+            options: {placeholder: '00000-000'}
+          }
+        }
+      }
     ),
     methods: Object.assign({},
       mapActions('lib/', ['update-contact']),
       {
+        populateCEP(data){
+          this.getContact.data['ESTADO'] = data.uf;
+          this.getContact.data['CIDADE'] = data.localidade;
+          this.getContact.data['BAIRRO'] = data.bairro;
+          this.getContact.data['CEP'] = data.cep;
+          this.updateDataContact();
+        },
         updateDataContact(){
           this['update-contact'](this.getContact.data).then(ret => {
-            console.log(ret);
             CsEvents.$emit('showAlert', {
               success: ret.ok,
               text: ret.ok ? "Informações atualizadas!" : `Falha ao atualizar informações: ${ret.data.message}`,
